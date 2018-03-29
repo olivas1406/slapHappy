4d71b07142ecba5e3d1a4abc51a3cac66114833d
olivas1406 <olivas1406@msn.com>
2018-03-29 13:01:06 -0700

    updated CSS, added map of current location

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,32 +10,11 @@
 $(document).ready(function(){ 
 
 
-    /*
-    $(".mainImage").click(function() {                                  // Event listener, on click run function showMe
-        showMe();
-    });
-  
-    function showMe() {                                                 // Function to show images/link to main.html
-        $(".choose").text("What's Pissing You Off Today?");             // Show this text
-        $(".mainImage").remove();                                       // Remove the image that was clicked
-        $(".whatItIs").append("<a href='./main.html'><img src='./assets/images/boss.jpg' class='pic pic1' alt='Link to Main Page'></a>");
-        $(".whatItIs").append("<a href='./main.html'><img src='./assets/images/family.png' class='pic pic2' alt='Link to Main Page'></a>");
-        $(".whatItIs").append("<a href='./main.html'><img src='./assets/images/traffic.jpg' class='pic pic3' alt='Link to Main Page'></a>");
-        $(".whatItIs").append("<a href='./main.html'><img src='./assets/images/mamaFratelli.jpg' class='pic pic4' alt='Link to Main Page'></a>");
-    };                                                                  // Link these images to main.html
-  */
-
-/*
-    $("pic").on("click", function() {                                   // When you click on one of the pics
-        $("pic").hide();                                                // Hide all the pics
-    });
-*/
-
-
     
     $(".gym").hide();                                                   // Hide the map with local gyms
     $(".arcades").hide();                                               // Hide the map with local arcades
     $(".bars").hide();                                                  // Hide the map with local bars
+    $(".yourLocation").hide();
 
     $(".holdMe1").on("click", function() {                              // When you click on the first image
         $(".gym").show();                                               // Show the map with local gyms
@@ -50,6 +29,11 @@ $(document).ready(function(){
     $(".holdMe3").on("click", function() {                              // When you click on the third image
         $(".bars").show();                                              // Show the map with local bars
         $(".holdMe3").hide();                                           // Hide the image that was clicked
+    });
+
+    $(".holdMe4").on("click", function() {                              // When you click on the third image
+        $(".yourLocation").show();                                              // Show the map with local bars
+        $(".holdMe4").hide();                                           // Hide the image that was clicked
     });
     
 });
